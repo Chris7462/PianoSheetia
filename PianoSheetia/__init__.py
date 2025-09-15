ee460e6e111739a55d942bc395e8eb0fe508de71
chris7462 <chris7462@gmail.com>
2025-09-15 10:56:57 -0400

    add video downloader

--- PianoSheetia/__init__.py
+++ PianoSheetia/__init__.py
@@ -1,27 +1,26 @@
 """
-VidToSheet - Video to Sheet Music Conversion Tool
+PianoSheetia - Synthesia Piano Videos to Sheet Music
 
-A Python package for converting piano performance videos into sheet music
-by detecting piano keys and analyzing their positions over time.
+A Python package for converting synthesia piano videos into sheet music.
 """
 
 # Import main classes and functions
-#from .keyboard_detector import PianoKeyDetector
 from .piano_keyboard import PianoKeyboard, PianoKey
-#from .video_processor import VideoProcessor
+from .video_downloader import VideoDownloader
+#from .keyboard_detector import PianoKeyDetector
 
 # Package metadata
 __version__ = "0.1.0"
 __author__ = "Yi-Chen Zhang"
 __email__ = "chris7462@gmail.com"
-__description__ = "Convert piano performance videos to sheet music"
+__description__ = "Convert synthesia piano videos to sheet music"
 
 # Define what gets imported with "from VidToSheet import *"
 __all__ = [
-    #"PianoKeyDetector",
     "PianoKey",
     "PianoKeyboard",
-    #"VideoProcessor",
+    "VideoDownloader",
+    #"PianoKeyDetector",
 ]
 
 # Optional: Add convenience imports for common use cases
